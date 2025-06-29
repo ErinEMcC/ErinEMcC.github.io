c76eb6ecd5ddfdf0110dee584e27a15e91c68863
ErinEMcC <erinm35@gmail.com>
2025-06-29 15:40:04 -0400

    Update tuffet.js

--- js/tuffet.js
+++ js/tuffet.js
@@ -1,41 +1,24 @@
-function createGrammar(RiTa, context) {
-  // Create the grammar rules
-  var animals = require('.data/animals.json').animals;
-  // a helper function that we add to all arrays
-  // this picks a random element from an array
-  Array.prototype.pick = function() {
-    return this[Math.floor(Math.random()*this.length)];
-  };
-  
+async function createGrammar(RiTa, context) {
+  // Fetch JSON from your published GitHub Pages URL
+  const response = await fetch("./data/animals.json");
+  const json = await response.json();
+  const animals = json.animals;
+
   const rules = {
-    // The starting rule - this is what gets expanded first
-    // The pipe symbol | provides alternatives
-    start: "<h2>My name is Erin</h2>$line1<br>$line2<br>$line3<br>$line4<br>$line5<br>$line6<br>,
+    "start": "<h2>My name is Erin</h2><br>$line2<br>$line3<br>$line4<br>$line5<br>$line6<br>",
+   
+    "line2": "I'm a $librarian",
+    "line3": "$gerund my $gizmo",
+    "line4": "There came a $adjective $animal",
+    "line5": "with a secret $noun",
+    "line6": "And frightened Miss Muffet away.",
     
-    line1: "My name is Erin",
-    line2: "I'm a $librarian",
-    line3: "$verbGerund my $.oneSyllableNoun and $.oneSyllableNoun",
-    line4: "There can a big $spider",
-    line5: "That sat down $beside her",
-    line6: "And frightened Miss Muffet away.",
-    
-    // Set up some repeatable words
-    
-    "#librarian = myJobs[randomIndex]",
-    
-    
+    "animal": animals,
+    "adjective": ["big", "small", "fierce", "cuddly", "striped"],
+    "noun": ["plan", "letter", "story", "document", "recipe"]
   };
 
-  // Important: Return both the rules and the context
   return rules;
 }
 
-// Export the function so it can be imported in other files
 export default createGrammar;
-
-//Little Miss Muffet
-//Sat on a tuffet,
-//Eating her curds and whey;
-//There came a big spider,
-//Who sat down beside her
-//And frightened Miss Muffet away.
